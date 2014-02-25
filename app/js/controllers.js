0e07f6e69fba8a30eef48c41a04a6b04a72de8b1
randver <randver10@ru.is>
2014-02-25 11:28:32 +0000

    bla

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,37 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('MyCtrl1', ["$scope", function($scope) {
+  	$scope.userlist = [];
 
+
+  	var socket = io.connect("http://localhost:8080")
+  	$scope.loggedIn = false;
+  	socket.on("roomlist", function(data){
+  		console.log(data);
+  		$scope.$apply(function(){
+  			  $scope.rooms = data;	
+  		})
+
+  	});
+
+  	$scope.rooms = [];
+
+  	$scope.submit = function() {
+  		var user = $scope.username;
+  		console.log(user);
+	  	socket.emit("adduser", user, function(available){
+	    if (available){
+	        // The "dabs" username is not taken! 
+	        socket.emit("rooms");
+	        $scope.loggedIn = true;
+	    }
+	    else
+	    {
+	    	alert(user + " is taken, please select a new username");
+	    }
+		});
+	}
   }])
   .controller('MyCtrl2', [function() {
 
